f4b339609b011e3bf212267217f3126df17aff3f
heychirag <i@chira.ga>
2017-07-08 13:42:37 -0600

    database added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Initialising....");
+
+var db = require('mongoose');
+
+var DB_URI = "mongodb://instabot:pulsung7690@ds151222.mlab.com:51222/heroku_t3fqxl7f";
+
+db.connect(DB_URI, function (error) {
+  if (error) console.error(error);
+  else console.log('Database Connected');
+});
+
